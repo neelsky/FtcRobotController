28d06ba5051cbee284f7c73aebac5f8b6f1e90de
neelsky <neelsuley@gmail.com>
2026-09-25 16:07:42 -0700

    BASIC MOVEMENT

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Neel/BasicMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Neel/BasicMovement.java
@@ -1,4 +1,90 @@
 package org.firstinspires.ftc.teamcode.Neel;
 
-public class BasicMovement {
+
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@TeleOp(name = "Red FTC Comp", group = "Red Ftc Comp")
+public class BasicMovement extends LinearOpMode {
+
+
+    private DcMotor frontLeftMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor backRightMotor;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        waitForStart();
+
+
+        while (opModeIsActive()) {
+            if (isStopRequested()) return;
+            telemetry.addData("frontRight", frontRightMotor.getCurrentPosition());
+
+            telemetry.addData("backLeft", backLeftMotor.getCurrentPosition());
+
+            telemetry.addData("backRight", backRightMotor.getCurrentPosition());
+
+
+            telemetry.addData("frontLeft", frontLeftMotor.getCurrentPosition());
+
+
+
+            doDriving();
+
+
+            telemetry.update();
+        }
+    }
+
+
+
+    private void doDriving() {
+        double y = -gamepad1.left_stick_y;
+        double x = gamepad1.left_stick_x * 1.1;
+        double rx = gamepad1.right_stick_x;
+
+
+
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (y + x + rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
+
+        //todo: enable this.
+        frontLeftMotor.setPower(frontLeftPower);
+        backLeftMotor.setPower(backLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+        backRightMotor.setPower(backRightPower);
+    }
+
+    private void initializeMotors() {
+        frontLeftMotor = hardwareMap.dcMotor.get("frontLeft");
+        backLeftMotor = hardwareMap.dcMotor.get("backLeft");
+        frontRightMotor = hardwareMap.dcMotor.get("frontRight");
+        backRightMotor = hardwareMap.dcMotor.get("backRight");
+
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        backRightMotor.setDirection(DcMotor.Direction.FORWARD);
+
+
+
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Neel/LimelightTest.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.Neel;
+
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.LLStatus;
+
+public class LimelightTest {
+}
